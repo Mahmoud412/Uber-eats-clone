0a477cf315d431a349e08173c59460c1e298f627
Mahmoud412 <52178976+Mahmoud412@users.noreply.github.com>
2022-09-10 16:27:31 +0200

    Added Categories

--- screens/Home.js
+++ screens/Home.js
@@ -2,14 +2,16 @@ import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 import HeaderTabs from "../componets/HeaderTabs";
 import SearchBar from "../componets/SearchBar";
+import Categories from "../componets/Categories";
 
 export default function Home() {
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
-        <SearchBar/>
+        <SearchBar />
       </View>
+      <Categories />
     </SafeAreaView>
   );
 }
