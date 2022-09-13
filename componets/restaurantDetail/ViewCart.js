209fc46f231170d50b1d2af8d02f27c36db4a150
Mahmoud412 <52178976+Mahmoud412@users.noreply.github.com>
2022-09-13 13:13:23 +0200

    added modal to view cart button

--- componets/restaurantDetail/ViewCart.js
+++ componets/restaurantDetail/ViewCart.js
@@ -1,8 +1,17 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Modal,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function ViewCart() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   const items = useSelector((state) => state.cartReducer.selectedItems.items);
   const total = items
     .map((item) => Number(item.price.replace("$", "")))
@@ -13,13 +22,37 @@ export default function ViewCart() {
     currency: "USD",
   });
 
-  console.log(totalUSD);
   return (
     <>
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Pressable
+                style={[styles.buttonModal, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
       {total ? (
         <View style={styles.Container}>
           <View style={styles.subContainer}>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity
+              onPress={() => setModalVisible(true)}
+              style={styles.button}
+            >
               <Text style={{ color: "white", fontSize: 20, marginRight: 40 }}>
                 View Cart
               </Text>
@@ -58,5 +91,43 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 840,
     zIndex: 999,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonModal: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "black",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
